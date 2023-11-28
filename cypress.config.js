55aa0f825359856d9cd87f8d8ce44e8ceff15459
Maria Fernanda <fernanda.jimenezh@pragma.com.co>
2023-11-27 19:36:07 -0500

    Se crea el primer feture de compra

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,23 @@
 const { defineConfig } = require("cypress");
-import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor') 
 const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
 const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://teststore.automationtesting.co.uk",
     specPattern: "**/*.feature",
+
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
-      on("file:preprocessor", createBundler(
-        {
-        plugins: [createEsbuildPlugin(config)],
-      }));
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
 
       return config;
-    }
+    },
   },
 });
